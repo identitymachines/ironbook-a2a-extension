966da562c4e12b9600ba28be22cea3a87c57b1d9
matt-forum <matt@forumvc.com>
2025-08-25 15:56:35 -0400

    Initial extension

---
+++ src/ironbook_a2a/__init__.py
@@ -0,0 +1,16 @@
+from a2a.types import AgentExtension
+
+_CORE_PATH = 'github.com/identitymachines/ironbook-a2a-extension/v1'
+IRONBOOK_EXTENSION_URI = f'https://{_CORE_PATH}'
+IRONBOOK_AUTH_TOKEN_FIELD = f'{_CORE_PATH}/auth-token'
+IRONBOOK_CONTEXT_FIELD = f'{_CORE_PATH}/context'
+
+class IronBookExtension:
+    def agent_extension(self) -> AgentExtension:
+        """Get the AgentExtension representing this extension."""
+        return AgentExtension(
+            uri=IRONBOOK_EXTENSION_URI,
+            description='Enforces IronBook policies',
+        )
+
+__all__ = ['IronBookExtension', 'IRONBOOK_EXTENSION_URI', 'IRONBOOK_AUTH_TOKEN_FIELD', 'IRONBOOK_CONTEXT_FIELD']
